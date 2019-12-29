f0351531c560d3a47615ee102d046e56222a25c7
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2019-12-29 15:53:55 +0100

    Update start.sh
    Should not cd to the directory user is already in.

--- MinecraftServer/start.sh
+++ MinecraftServer/start.sh
@@ -1,3 +1,2 @@
 # Start minecraft_server with 2600MB ram
-cd /home/pi/minecraft/
 /usr/bin/screen -dmS minecraft_server /usr/bin/java -jar -Xms2600M -Xmx2600M /home/pi/minecraft_server/latest_paper.jar
