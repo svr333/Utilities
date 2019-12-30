47b9374b0bfb083f67fc54bcdd35a6983f0b4314
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2019-12-30 14:59:50 +0100

    Lower ram to 2Gigs

--- MinecraftServer/start.sh
+++ MinecraftServer/start.sh
@@ -1,2 +1,2 @@
 # Start minecraft_server with 2600MB ram
-/usr/bin/screen -dmS minecraft_server /usr/bin/java -jar -Xms2600M -Xmx2600M /home/pi/minecraft_server/latest_paper.jar
+/usr/bin/screen -dmS minecraft_server /usr/bin/java -jar -Xms2000M -Xmx2000M /home/pi/minecraft_server/latest_paper.jar
