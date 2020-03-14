2f2f2fd9328c08688df452a544d2fee91bda7068
Senne Van Rompaey <vanrompaey.senne@protonmail.com>
2020-03-14 19:00:35 +0100

    Fix mistake in comment

--- MinecraftServer/start.sh
+++ MinecraftServer/start.sh
@@ -1,2 +1,2 @@
-# Start minecraft_server with 2600MB ram
+# Start minecraft_server with 2000MB ram
 /usr/bin/screen -dmS minecraft_server /usr/bin/java -jar -Xms2000M -Xmx2000M /home/pi/minecraft_server/latest_paper.jar
